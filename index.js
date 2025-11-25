d3869a52e7acd738c8ce92b9e0863517c28b7022
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-11-25 16:04:28 +0200

    feat: add routes for daily exercises and exercises management

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import registerRouter from "./src/routes/register.js";
 import loginRouter from "./src/routes/login.js";
 import { initDb } from "./src/db/connect.js";
+import "./src/routes/daily.js";
+import "./src/routes/exercise.js";
 
 dotenv.config();
 
@@ -22,6 +24,10 @@ app.get("/", (req, res) => {
 app.use("/api/register", registerRouter);
 
 app.use("/api/login", loginRouter);
+
+app.use("/api/daily", dailyRouter);
+
+app.use("/api/exercises", exerciseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
