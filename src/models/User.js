0d9d7473fbb1facbc910024386dc00090508b671
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-11-26 16:27:52 +0200

    fix: streamline password hashing middleware by removing unnecessary next() calls

--- src/models/User.js
+++ src/models/User.js
@@ -13,13 +13,12 @@ const userSchema = new Schema(
 
 // Hash password before saving(used for register)
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+  if (!this.isModified("password")) return;
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
   } catch (err) {
-    next(err);
+    throw err;
   }
 });
 
